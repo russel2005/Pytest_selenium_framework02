2b46858cf40b2f03ed4a99b41c3834e4a6a79608
russel2005 <russel2005@gmail.com>
2020-07-11 11:49:28 -0400

    modified file test_parameterize.py

--- tests/pytestpackage/test_parameterize.py
+++ tests/pytestpackage/test_parameterize.py
@@ -5,7 +5,18 @@ test_data = [[1, 2, 2],
              [2, 4, 8],
              [3, 3, 9]]
 
+''' if you want to run parametriza test and require to run one specific method onetime only like setup() method'''
+setup = 1
+
+
+def setup():
+    print("this is setup")
+    global setup
+    setup = 0
+
 
 @pytest.mark.parametrize("a, b, e", test_data)
 def test_mul(a, b, e):
+    if setup:
+        setup
     assert a*b == e
