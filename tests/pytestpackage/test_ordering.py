07ddd8107092b9ab074c2d1f4cc41ce528192c9e
russel2005 <russel2005@gmail.com>
2020-07-11 11:29:19 -0400

    modified file test_ordering.py for dependency and added plugins

--- tests/pytestpackage/test_ordering.py
+++ tests/pytestpackage/test_ordering.py
@@ -1,25 +1,26 @@
 '''
 pip install pytest-ordering
+pip install pytest-dependency
 '''
-'''
-def test_bar():
-    assert True
 
-
-def test_foo():
-    assert True
-
-'''
 import pytest
 
-@pytest.mark.run(order=2)
-def test_foo():
-    assert True
 
-@pytest.mark.run(order=1)
-def test_bar():
-    assert True
+class Test():
 
-@pytest.mark.run(order=3)
-def test_three():
-    assert True
+    @pytest.mark.run(order=2)
+    def test_foo(self):
+        assert True
+
+    #@pytest.mark.run(order=1)
+    @pytest.mark.dependency()
+    def test_bar(self):
+        assert True
+
+    @pytest.mark.run(order=3)
+    def test_three(self):
+        assert True
+
+    @pytest.mark.dependency(depends=['Test::test_bar'])
+    def test_sample01(self):
+        assert True
